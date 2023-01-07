251fb798e3fcf43a26d4996bb72f2a62add67670
yogalakshmichandran <yogalakshmichandran@gmail.com>
2023-01-07 16:44:26 +0530

    Ticket status dynamic view

--- src/app/helpdesk/it-support/list-it-support/notificationpop/notificationpop.component.ts
+++ src/app/helpdesk/it-support/list-it-support/notificationpop/notificationpop.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { HttpServiceService } from 'src/app/auth/http-service.service';
+import { ItSupportresultbean } from '../../it-support-result-bean';
+import { Itsupportservice } from '../../it-support.service';
 
 @Component({
   selector: 'app-notificationpop',
@@ -6,10 +10,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./notificationpop.component.sass']
 })
 export class NotificationpopComponent implements OnInit {
-
-  constructor() { }
+  closeCountValue: any;
+  AssignedCountValue: any;
+  OpenedCountValue: any;
+  HoldCountValue: any;
+  constructor(
+   private itsupportservice:Itsupportservice,
+   public httpClient: HttpClient,
+   private httpService:HttpServiceService
+   
+  ) { }
 
   ngOnInit(): void {
+
+
+    this.httpService.get<ItSupportresultbean>(this.itsupportservice.closedListCountUrl).subscribe(
+      (data) => {
+        console.log(data);
+        this.closeCountValue = data.closedListCount;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.name + " " + error.message);
+      }
+    );
+
+    this.httpService.get<ItSupportresultbean>(this.itsupportservice.openListCountUrl).subscribe(
+      (data) => {
+        console.log(data);
+        this.OpenedCountValue = data.openedListCount;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.name + " " + error.message);
+      }
+
+    );
+    this.httpService.get<ItSupportresultbean>(this.itsupportservice.holdListCountUrl).subscribe(
+      (data) => {
+        console.log(data);
+        this.HoldCountValue = data.holdListCount;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.name + " " + error.message);
+      }
+    );
+
+    this.httpService.get<ItSupportresultbean>(this.itsupportservice.AssignedListCountUrl).subscribe(
+      (data) => {
+        console.log(data);
+        this.AssignedCountValue = data.assignedListCount;
+      },
+      (error: HttpErrorResponse) => {
+        console.log(error.name + " " + error.message);
+      }
+    );
   }
 
 }
