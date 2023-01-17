cdea052b93ef859e8afb38913e916200d47fd06b
Gokulnath22 <85093912+Gokulnath22@users.noreply.github.com>
2023-01-17 19:59:29 +0530

    purchase-invoice

--- src/app/inventory/purchase-invoice/add-purchase-invoice/add-purchase-invoice.component.ts
+++ src/app/inventory/purchase-invoice/add-purchase-invoice/add-purchase-invoice.component.ts
@@ -12,6 +12,7 @@ import { TokenStorageService } from 'src/app/auth/token-storage.service';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import { PurchaseOrderService } from '../../purchase-order/purchase-order.service';
+import * as moment from 'moment';
 
 export const MY_DATE_FORMATS = {
   parse: {
@@ -29,8 +30,8 @@ export const MY_DATE_FORMATS = {
   selector: 'app-add-purchase-invoice',
   templateUrl: './add-purchase-invoice.component.html',
   styleUrls: ['./add-purchase-invoice.component.sass'],
-   // Date Related code
-   providers: [
+  // Date Related code
+  providers: [
     { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
     {
       provide: MAT_DATE_FORMATS, useValue: {
@@ -50,20 +51,11 @@ export class AddPurchaseInvoiceComponent implements OnInit {
   edit: boolean = false;
   requestId: any;
   purchaseInvoiceDetailList = [];
-  hide3 = true;
-  agree3 = false;
-  dataarray = [];
-  custList = [];
-  DeliveryNoList = [];
   itemList = [];
   uomList = [];
-  lpoList = [];
   vendorList = [];
-  fetchLpoList = [];
-  purchaseOrderNumberList= [];
-  value1: number;
-  value2: number;
-  value3: number;
+  purchaseOrderNumberList = [];
+  locationList = [];
 
   constructor(private fb: FormBuilder,
     public router: Router,
@@ -75,24 +67,25 @@ export class AddPurchaseInvoiceComponent implements OnInit {
     private commonService: CommonService,
     private spinner: NgxSpinnerService,
     private snackBar: MatSnackBar,
-    private purchaseOrderService:PurchaseOrderService) {
+    private purchaseOrderService: PurchaseOrderService) {
 
     this.docForm = this.fb.group({
       purchaseInvoiceNo: [""],
       purchaseInvoiceId: [""],
-      purchaseInvoiceDate: [""],
-      purchaseInvoiceDateObj: [""],
-      partyInvoiceNo: [""],
-      purchaseOrderNo: [""],
+      purchaseInvoiceDate: [moment().format('DD/MM/YYYY')],
+      purchaseInvoiceDateObj: [moment().format('YYYY-MM-DD'), [Validators.required]],
+      partyInvoiceNo: ["", [Validators.required]],
+      purchaseOrderNo: ["", [Validators.required]],
       partyInvoiceDate: [""],
       partyInvoiceDateObj: [""],
-      dueDateObj: [""],
-      dueDate: [""],
-      companyName: ["ASSETCHEK"],
-      narration: [""],
-      vendor: [""],
+      dueDate: [moment().format('DD/MM/YYYY')],
+      dueDateObj: [moment().format('YYYY-MM-DD'), [Validators.required]],
+      description: [""],
+      total: [""],
+      vendorId: ["", [Validators.required]],
       amount: [""],
-      currency: [""],
+      locationId: ["", [Validators.required]],
+      currencyId: ["", [Validators.required]],
       exchangerate: [""],
       loginedUser: this.tokenStorage.getUserId(),
 
@@ -112,8 +105,8 @@ export class AddPurchaseInvoiceComponent implements OnInit {
   }
 
   ngOnInit() {
-     //PurchaseOrderNumber Dropdown List
-     this.httpService.get<any>(this.commonService.getPurchaseOrderNumberDropdown).subscribe({
+    //PurchaseOrderNumber Dropdown List
+    this.httpService.get<any>(this.commonService.getPurchaseOrderNumberDropdown).subscribe({
       next: (data) => {
         this.purchaseOrderNumberList = data;
       },
@@ -129,6 +122,17 @@ export class AddPurchaseInvoiceComponent implements OnInit {
       error: (error) => {
       }
     });
+
+    //Location Dropdown List
+    this.httpService.get<any>(this.commonService.getLocationDropdown).subscribe({
+      next: (data) => {
+        this.locationList = data;
+      },
+      error: (error) => {
+      }
+    });
+
+    
 
     //Currency  Dropdown List
     this.httpService.get<any>(this.commonService.getCurrencyDropdown).subscribe({
@@ -221,7 +225,7 @@ export class AddPurchaseInvoiceComponent implements OnInit {
     this.purchaseInvoiceService.editPurchaseInvoice(obj).subscribe({
       next: (res: any) => {
         this.spinner.hide();
-        let hpurchaseInvoiceDate= this.commonService.getDateObj(res.purchaseInvoice.purchaseInvoiceDate);
+        let hpurchaseInvoiceDate = this.commonService.getDateObj(res.purchaseInvoice.purchaseInvoiceDate);
         let hpartyInvoiceDate = this.commonService.getDateObj(res.purchaseInvoice.partyInvoiceDate);
         let hdueDate = this.commonService.getDateObj(res.purchaseInvoice.dueDate);
 
@@ -229,35 +233,36 @@ export class AddPurchaseInvoiceComponent implements OnInit {
           'purchaseInvoiceId': res.purchaseInvoice.purchaseInvoiceId,
           'purchaseOrderNo': res.purchaseInvoice.purchaseOrderNo,
           'partyInvoiceNo': res.purchaseInvoice.partyInvoiceNo,
-          'companyName': res.purchaseInvoice.companyName,
+          'description': res.purchaseInvoice.description,
           'purchaseInvoiceDateObj': hpurchaseInvoiceDate,
           'purchaseInvoiceDate': res.purchaseInvoice.purchaseInvoiceDate,
           'partyInvoiceDateObj': hpartyInvoiceDate,
           'partyInvoiceDate': res.purchaseInvoice.partyInvoiceDate,
           'dueDateObj': hdueDate,
           'dueDate': res.purchaseInvoice.dueDate,
-          'vendor': res.purchaseInvoice.vendor,
+          'vendorId': res.purchaseInvoice.vendorId,
           'lpoNo': res.purchaseInvoice.lpoNo,
-          'currency': res.purchaseInvoice.currency,
+          'locationId': res.purchaseInvoice.locationId,
+          'currencyId': res.purchaseInvoice.currencyId,
           'amount': res.purchaseInvoice.amount,
-          'narration': res.purchaseInvoice.narration,
+          'total': res.purchaseInvoice.total,
           'exchangerate': res.purchaseInvoice.exchangerate,
         });
-      if(res.purchaseInvoiceDetailList!=null && res.purchaseInvoiceDetailList.length>=1){
-        let purchaseInvoiceDtlArray = this.docForm.controls.purchaseInvoiceDetailList as FormArray;
-        purchaseInvoiceDtlArray.removeAt(0);
-        res.purchaseInvoiceDetailList.forEach(element => {
+        if (res.purchaseInvoiceDetailList != null && res.purchaseInvoiceDetailList.length >= 1) {
           let purchaseInvoiceDtlArray = this.docForm.controls.purchaseInvoiceDetailList as FormArray;
-          let arraylen = purchaseInvoiceDtlArray.length;
-          let newUsergroup: FormGroup = this.fb.group({
-            itemId: [element.itemId],
-            qty: [element.qty],
-            uomid: [element.uomid],
-            quotePrice: [element.quotePrice]
-          })
-          purchaseInvoiceDtlArray.insert(arraylen, newUsergroup);
-        });
-      }
+          purchaseInvoiceDtlArray.removeAt(0);
+          res.purchaseInvoiceDetailList.forEach(element => {
+            let purchaseInvoiceDtlArray = this.docForm.controls.purchaseInvoiceDetailList as FormArray;
+            let arraylen = purchaseInvoiceDtlArray.length;
+            let newUsergroup: FormGroup = this.fb.group({
+              itemId: [element.itemId],
+              qty: [element.qty],
+              uomid: [element.uomid],
+              quotePrice: [element.quotePrice]
+            })
+            purchaseInvoiceDtlArray.insert(arraylen, newUsergroup);
+          });
+        }
       },
       error: (error) => {
         this.spinner.hide();
@@ -315,7 +320,6 @@ export class AddPurchaseInvoiceComponent implements OnInit {
     if (!this.edit) {
       this.docForm.reset();
       this.docForm.patchValue({
-        'companyName': 'ASSETCHEK',
         'loginedUser': this.tokenStorage.getUserId()
       })
     } else {
@@ -403,34 +407,36 @@ export class AddPurchaseInvoiceComponent implements OnInit {
     purchaseInvoiceDtlArray.removeAt(index);
   }
 
-  getPurchaseInvoiceDetails(POID:number) {
+  getPurchaseInvoiceDetails(POID: number) {
     if (POID != undefined && POID != null) {
       this.spinner.show();
-      this.httpService.get<any>(this.purchaseOrderService.getPurchaseOrderDetailsList + "?purchaseOrderId="+POID).subscribe({
+      this.httpService.get<any>(this.purchaseOrderService.getPurchaseOrderDetailsList + "?purchaseOrderId=" + POID).subscribe({
         next: (res: any) => {
-        this.spinner.hide();
-        if (res.success) {
-          if(res.purchaseOrderDetailList!=null && res.purchaseOrderDetailList.length>=1){
-            let purchaseInvoiceDtlArray = this.docForm.controls.purchaseInvoiceDetailList as FormArray;
-            purchaseInvoiceDtlArray.clear();
-            res.purchaseOrderDetailList.forEach(element => {
+          this.spinner.hide();
+          if (res.success) {
+            if (res.purchaseOrderDetailList != null && res.purchaseOrderDetailList.length >= 1) {
               let purchaseInvoiceDtlArray = this.docForm.controls.purchaseInvoiceDetailList as FormArray;
-              let arraylen = purchaseInvoiceDtlArray.length;
-              let newUsergroup: FormGroup = this.fb.group({
-                itemId: [element.itemId],
-                qty: [element.purchaseQty],
-                uomid: [element.purchaseUOM],
-                quotePrice: [element.price]
-              })
-              purchaseInvoiceDtlArray.insert(arraylen, newUsergroup);
-            });
+              purchaseInvoiceDtlArray.clear();
+              res.purchaseOrderDetailList.forEach(element => {
+                let purchaseInvoiceDtlArray = this.docForm.controls.purchaseInvoiceDetailList as FormArray;
+                let arraylen = purchaseInvoiceDtlArray.length;
+                let newUsergroup: FormGroup = this.fb.group({
+                  itemId: [element.itemId],
+                  qty: [element.purchaseQty],
+                  uomid: [element.purchaseUOM],
+                  quotePrice: [element.price]
+                })
+                purchaseInvoiceDtlArray.insert(arraylen, newUsergroup);
+              });
+            }
           }
+        },
+        error: (error) => {
+          this.spinner.hide();
         }
-      },
-      error: (error) => {
-        this.spinner.hide();
-      }
-    });
+      });
     }
   }
+
+
 }
