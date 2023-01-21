fd8b33a70cad6bf7dc14d22f620ccd8d6c345eda
Riyaz Ahamed K <riyazahamed1444@gmail.com>
2023-01-21 12:14:55 +0530

    payment history

---
+++ src/app/payments/payments-history/payments-history.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaymentsHistoryComponent } from './payments-history/payments-history.component';
+
+
+
+@NgModule({
+  declarations: [
+    PaymentsHistoryComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class PaymentsHistoryModule { }
