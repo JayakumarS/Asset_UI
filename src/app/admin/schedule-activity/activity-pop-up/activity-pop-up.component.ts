d9e6fb3d216b1dd0797cc2f9b0a39d9024c1ff1c
StephenVivek <100684744+StephenVivek@users.noreply.github.com>
2023-01-27 12:52:21 +0530

    activity header pop up

--- src/app/admin/schedule-activity/activity-pop-up/activity-pop-up.component.ts
+++ src/app/admin/schedule-activity/activity-pop-up/activity-pop-up.component.ts
@@ -1,223 +1,122 @@
-import { Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { HttpClient } from "@angular/common/http";
-import { MatDialog } from "@angular/material/dialog";
-import { MatPaginator } from "@angular/material/paginator";
-import { MatSort } from "@angular/material/sort";
-import { DataSource } from "@angular/cdk/collections";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { MatMenuTrigger } from "@angular/material/menu";
-import { BehaviorSubject, fromEvent, merge, Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SelectionModel } from "@angular/cdk/collections";
-import { UnsubscribeOnDestroyAdapter } from "src/app/shared/UnsubscribeOnDestroyAdapter";
-import { serverLocations } from 'src/app/auth/serverLocations';
 import { HttpServiceService } from 'src/app/auth/http-service.service';
-import { MainService } from '../../dashboard/main.service'; 
-import { main } from '../../dashboard/main-model'; 
-import { DeleteVendorComponent } from 'src/app/master/vendor/list-vendor/delete/delete.component';
+import { CommonService } from 'src/app/common-service/common.service';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
+import { AuditableAssetResultBean } from 'src/app/audit/auditable-asset/auditable-asset-result-bean'; 
+import { AuditableAssetService } from 'src/app/audit/auditable-asset/auditable-asset.service'; 
+import { AuditableAsset } from 'src/app/audit/auditable-asset/auditable-asset-model'; 
+import { NotificationService } from 'src/app/core/service/notification.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { ScheduleActivityMaster } from '../schedule-acvtivity.model';
 import * as moment from 'moment';
+import { MainService } from '../../dashboard/main.service';
+
+export const MY_DATE_FORMATS = {
+  parse: {
+    dateInput: 'DD/MM/YYYY',
+  },
+  display: {
+    dateInput: 'DD/MM/YYYY',
+    monthYearLabel: 'MMMM YYYY',
+    dateA11yLabel: 'LL',
+    monthYearA11yLabel: 'MMMM YYYY'
+  },
+};
 
 @Component({
   selector: 'app-activity-pop-up',
   templateUrl: './activity-pop-up.component.html',
-  styleUrls: ['./activity-pop-up.component.sass']
+  styleUrls: ['./activity-pop-up.component.sass'],
+  // Date Related code
+  providers: [
+    { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
+    { provide: MAT_DATE_FORMATS, useValue: {
+      display: {
+          dateInput: 'DD/MM/YYYY',
+          monthYearLabel: 'MMMM YYYY',
+      },
+  } },CommonService
+  ]
 })
-export class ActivityPopUpComponent extends UnsubscribeOnDestroyAdapter implements OnInit {
-  
-  displayedColumns = [
-    "activityTypeText",
-    "locationText",
-    "enddate",
-    "startdate",
-    //  "actions"
-   ];
- 
-   dataSource: ExampleDataSource | null;
-   exampleDatabase: MainService | null;
-   selection = new SelectionModel<main>(true, []);
-   index: number;
-   id: number;
-   customerMaster: main | null;
-  spinner: any;
-  todayDate: string;
-   constructor(
-     public httpClient: HttpClient,
-     public dialog: MatDialog,
-     public vendorService: MainService,
-     private snackBar: MatSnackBar,
-     private router: Router,
-     private serverUrl:serverLocations,
-     private httpService:HttpServiceService,
-   ) {
-     super();
-   }
- 
-   @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-   @ViewChild(MatSort, { static: true }) sort: MatSort;
-   @ViewChild("filter", { static: true }) filter: ElementRef;
-   @ViewChild(MatMenuTrigger)
-   contextMenu: MatMenuTrigger;
-   contextMenuPosition = { x: "0px", y: "0px" };
- 
-   ngOnInit(): void {
-     this.onSubmit();
+export class ActivityPopUpComponent implements OnInit {
+  docForm: FormGroup;
+  scheduleActivityMaster:ScheduleActivityMaster;
+  requestId: any;
+  edit:boolean=false;
+  assetList:[];
+  currencyList: [];
+  assetTypeList:[];
+  depreciationMethodList:[];
+  getAuditableAssetDetails:[];
+  scheduleActivityDetails:[];
+  fullLifeFlag:boolean=false;
 
-     
-   }
-
-   onSubmit(){
-    this.todayDate=moment().format('YYYY-MM-DD');
-    console.log(this.todayDate);
-    this.loadData();
-}
- 
-   refresh(){
-     this.loadData();
-   }
- 
-   public loadData() {
-     this.exampleDatabase = new MainService(this.httpClient,this.serverUrl,this.httpService);
-     this.dataSource = new ExampleDataSource(
-       this.exampleDatabase,
-       this.paginator,
-       this.sort,
-       this.todayDate
-     );
-     this.subs.sink = fromEvent(this.filter.nativeElement, "keyup").subscribe(
-       () => {
-         if (!this.dataSource) {
-           return;
-         }
-         this.dataSource.filter = this.filter.nativeElement.value;
-       }
-     );
-   }
- 
- 
-   editCall(row) {
- 
-     this.router.navigate(['/master/vendor/addVendor/'+row.vendorId]);
- 
-   }
-
-
-
-
- 
- 
-   private refreshTable() {
-     this.paginator._changePageSize(this.paginator.pageSize);
-   }
- // context menu
-   onContextMenu(event: MouseEvent, item: main) {
-     event.preventDefault();
-     this.contextMenuPosition.x = event.clientX + "px";
-     this.contextMenuPosition.y = event.clientY + "px";
-     this.contextMenu.menuData = { item: item };
-     this.contextMenu.menu.focusFirstItem("mouse");
-     this.contextMenu.openMenu();
-   }
-   
-   showNotification(colorName, text, placementFrom, placementAlign) {
-     this.snackBar.open(text, "", {
-       duration: 2000,
-       verticalPosition: placementFrom,
-       horizontalPosition: placementAlign,
-       panelClass: colorName,
-     });
-   }
-}
-
-export class ExampleDataSource extends DataSource<main> {
-  filterChange = new BehaviorSubject("");
-  get filter(): string {
-    return this.filterChange.value;
-  }
-  set filter(filter: string) {
-    this.filterChange.next(filter);
-  }
-  filteredData: main[] = [];
-  renderedData: main[] = [];
-  constructor(
-    public exampleDatabase: MainService,
-    public paginator: MatPaginator,
-    public _sort: MatSort,
-    public todayDate
-  ) {
-    super();
-    // Reset to the first page when the user changes the filter.
-    this.filterChange.subscribe(() => (this.paginator.pageIndex = 0));
-  }
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<main[]> {
-    // Listen for any changes in the base data, sorting, filtering, or pagination
-    const displayDataChanges = [
-      this.exampleDatabase.dataChange,
-      this._sort.sortChange,
-      this.filterChange,
-      this.paginator.page,
-    ];
-    this.exampleDatabase.getAllList(this.todayDate);
-    return merge(...displayDataChanges).pipe(
-      map(() => {
-        // Filter data
-        this.filteredData = this.exampleDatabase.data
-          .slice()
-          .filter((commodity: main) => {
-            const searchStr = (
-              commodity.activityTypeText +
-              commodity.locationText +
-              commodity.startdate +
-              commodity.enddate 
-             
-            ).toLowerCase();
-            return searchStr.indexOf(this.filter.toLowerCase()) !== -1;
-          });
-        // Sort filtered data
-        const sortedData = this.sortData(this.filteredData.slice());
-        // Grab the page's slice of the filtered sorted data.
-        const startIndex = this.paginator.pageIndex * this.paginator.pageSize;
-        this.renderedData = sortedData.splice(
-          startIndex,
-          this.paginator.pageSize
-        );
-        return this.renderedData;
-      })
-    );
-  }
-  disconnect() {}
-  /** Returns a sorted copy of the database data. */
-  sortData(data: main[]): main[] {
-    if (!this._sort.active || this._sort.direction === "") {
-      return data;
-    }
-    return data.sort((a, b) => {
-      let propertyA: number | string = "";
-      let propertyB: number | string = "";
-      switch (this._sort.active) {
-        // case "id":
-        //   [propertyA, propertyB] = [a.id, b.id];
-        //   break;
-        case "activityTypeText":
-          [propertyA, propertyB] = [a.activityTypeText, b.activityTypeText];
-          break;
-        // case "vendorCountryName":
-        //   [propertyA, propertyB] = [a.vendorCountryName, b.vendorCountryName];
-        //   break;
-        case "locationText":
-          [propertyA, propertyB] = [a.locationText, b.locationText];
-          break;
-        case "enddate":
-            [propertyA, propertyB] = [a.enddate, b.enddate];
-          break;  
-        
-      }
-      const valueA = isNaN(+propertyA) ? propertyA : +propertyA;
-      const valueB = isNaN(+propertyB) ? propertyB : +propertyB;
-      return (
-        (valueA < valueB ? -1 : 1) * (this._sort.direction === "asc" ? 1 : -1)
-      );
+  constructor(private fb: FormBuilder,
+    public auditableAssetService:AuditableAssetService,
+    private httpService: HttpServiceService,
+    private snackBar:MatSnackBar,
+    private router:Router,private cmnService:CommonService,
+    private commonService: CommonService,
+    private notificationService: NotificationService,
+    public route: ActivatedRoute,public mainService:MainService,
+    public dialogRef: MatDialogRef<ActivityPopUpComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,) 
+    {
+    this.docForm = this.fb.group({
+      activityTypeText:[""],
+      locationText:[""],
     });
   }
+
+  ngOnInit(): void {
+
+
+  this.docForm = this.fb.group({
+    activityTypeText:[""],
+    locationText:[""],
+  });
+
+  this.httpService.get<AuditableAssetResultBean>(this.mainService.getActivityPopUpUrl + "?todayDate=" + moment().format('YYYY-MM-DD')).subscribe((res: any) => {
+    this.scheduleActivityDetails = res.scheduleActivityDetails;
+    },
+    (err: HttpErrorResponse) => {
+       // error code here
+    }
+  );
+  
+  this.route.params.subscribe(params => {
+   if(params.id!=undefined && params.id!=0){
+    this.requestId = params.id;
+    this.edit=true;
+    this.fetchDetails(this.requestId) ;
+
+   }
+  });
+
+}
+
+  showNotification(colorName, text, placementFrom, placementAlign) {
+    this.snackBar.open(text, "", {
+      duration: 2000,
+      verticalPosition: placementFrom,
+      horizontalPosition: placementAlign,
+      panelClass: colorName,
+    });
+  }
+
+  fetchDetails(id: any): void {
+  }
+
+
+  onCancel(){
+    this.dialogRef.close();
+   }
+  
+
+  
 }
--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -97,10 +97,16 @@ export class SigninComponent
                 this.tokenStorage.saveCompanies(data.userDetails.companies);
                 this.tokenStorage.saveRoles(data.userDetails.roles);
 
-
+                // if(data.userDetails.companies.length>0){
+                //   this.showPopUp();
+                // }
                 this.loading = false;
-                this.router.navigate(["/admin/dashboard/main"]);
-                //  this.router.navigate(["/asset/assetMaster/listAssetMaster"]);
+                if(data.userDetails.roleId == 1 || data.userDetails.roleId == 2){
+                  this.router.navigate(["/admin/dashboard/main"]);
+                }
+                else if(data.userDetails.roleId == 3){
+                  this.router.navigate(["/asset/assetMaster/listAssetMaster"]);
+                }
               }, 1000);
               }else{
                  this.submitted = false;
