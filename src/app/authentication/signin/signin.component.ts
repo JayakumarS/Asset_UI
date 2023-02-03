1528cc95617e842539a32e1e60c0252b6f41a68d
varshini-1298 <varshiniarun@paragondynamics.in>
2023-02-03 10:48:00 +0530

    redirect user to sub page for user role

--- src/app/authentication/signin/signin.component.ts
+++ src/app/authentication/signin/signin.component.ts
@@ -39,9 +39,7 @@ export class SigninComponent
     private authService: AuthService,
     private app:AppService,
     private tokenStorage: TokenStorageService,
-    public dialog: MatDialog,
-
-    
+    public dialog: MatDialog
   ) {
     super();
   }
@@ -98,25 +96,26 @@ export class SigninComponent
                 this.tokenStorage.saveBranchId(data.userDetails.branchId);
                 this.tokenStorage.saveCompanies(data.userDetails.companies);
                 this.tokenStorage.saveRoles(data.userDetails.roles);
-               
+
                 // if(data.userDetails.companies.length>0){
                 //   this.showPopUp();
                 // }
                 this.loading = false;
-                this.loginSuccessUserLog();
                 if(data.userDetails.roleId == 1 || data.userDetails.roleId == 2 || data.userDetails.roleId == 3){
                   this.router.navigate(["/admin/dashboard/main"]);
                 }
                 else if(data.userDetails.roleId == 4){
                   this.router.navigate(["/asset/assetMaster/listAssetMaster"]);
-                }else{
-                  this.router.navigate(["/admin/dashboard/main"]);
                 }
+                else if (data.userDetails.roleId == 6) {
+                  this.router.navigate(["/payments/initiatePayment/subscription"]);
+              }
+                 
               }, 1000);
               }else{
                  this.submitted = false;
                   this.loading = false;
-                  this.error = data.message;
+                  this.error = "Invalid Login";
                 console.log(data.message);
               }
 
@@ -164,16 +163,6 @@ export class SigninComponent
       //   );
     }
   }
-  loginSuccessUserLog() {
-    const obj = {
-      username: this.authForm.value.username   }
-    console.log(obj);
-    this.authService.getSuccessuserLog(obj).subscribe((result: any) => {
-      
-    });
-  }
-
-
 
 
 }
