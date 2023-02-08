50af5fb5fa9293ebf689942d8f1b8926e59336c5
Riyaz Ahamed K <riyazahamed1444@gmail.com>
2023-02-08 14:24:46 +0530

    Scrap value decimal

--- src/app/asset/asset-master/add-asset-master/add-asset-master.component.ts
+++ src/app/asset/asset-master/add-asset-master/add-asset-master.component.ts
@@ -530,7 +530,7 @@ export class AddAssetMasterComponent
           'purchasePrice': res.addAssetBean.purchasePrice,
           'remarks': res.addAssetBean.remarks,
           'assetUser':res.addAssetBean.assetUser,
-          'scrapValue': res.addAssetBean.scrapValue,
+          'scrapValue': res.addAssetBean.scrapValue.toFixed(2),
           'selfOrPartner': res.addAssetBean.selfOrPartner,
           'serialNo': res.addAssetBean.serialNo,
           'transferredTo': parseInt(res.addAssetBean.transferredTo),
