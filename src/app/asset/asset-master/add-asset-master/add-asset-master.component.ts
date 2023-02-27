715fb935d54a69aa574d1bbe83bb8598a44008b7
Riyaz Ahamed K <riyazahamed1444@gmail.com>
2023-02-27 09:45:34 +0530

    category

--- src/app/asset/asset-master/add-asset-master/add-asset-master.component.ts
+++ src/app/asset/asset-master/add-asset-master/add-asset-master.component.ts
@@ -262,7 +262,7 @@ export class AddAssetMasterComponent
 
 
 
-    this.httpService.get<any>(this.commonService.getCategoryDropdown).subscribe({
+    this.httpService.get<any>(this.commonService.getCategoryDropdown+"?companyId="+this.tokenStorage.getCompanyId()).subscribe({
       next: (data) => {
         this.categoryList = data;
       },
