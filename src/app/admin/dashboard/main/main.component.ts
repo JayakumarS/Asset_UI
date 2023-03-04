5d5687fb4d02a32fd5deff7535c3e0be7a6e0369
StephenVivek <100684744+StephenVivek@users.noreply.github.com>
2023-03-04 10:38:35 +0530

    Dashboard changes

--- src/app/admin/dashboard/main/main.component.ts
+++ src/app/admin/dashboard/main/main.component.ts
@@ -73,7 +73,9 @@ export class MainComponent implements OnInit {
   columnOuterValueArray = [];
   columnInnerValueArray = [];
   userLogListDashboard = [];
-  testArray = [];
+  // Drilldown array
+  chartOptionsColumnChart: Highcharts.Options;
+  namePushingArray = [];
   //Donut Array
   donutName = [];
   donutValue = [];
@@ -399,33 +401,45 @@ configUserLog: {
       }else{
         this.pieChartFlag=false;
       }
+
       // Column with Drilldown Chart
       if(doughnutChartData.getOuterColumnChart.length!=0){
+       // **For Outer value 
       this.columnOuterValueArray=doughnutChartData.getOuterColumnChart;
-      this.chartOptionsColumnChart.series[0] = {
-        type: 'column',
-        data: this.columnOuterValueArray
-      }  
-      // this.chartOptionsColumnChart.drilldown.series[0] = {
-      //         name: 'Internet',
-      //         id: 'Vehicles',
-      //         type:'column',
-      //         data: [
-      //             [
-      //                 'v11.0',
-      //                 6.2
-      //             ],
-      //             [
-      //                 'v10.0',
-      //                 0.29
-      //             ],
-      //             [
-      //                 'v9.0',
-      //                 0.27
-      //             ]
-      //         ]
-      // }
-      this.columnInnerValueArray=doughnutChartData.getInnerColumnChart;
+
+      // **For Inner Value
+      // Spliting into array
+      for(let i=0;i<doughnutChartData.getInnerColumnChart.length;i++){
+        let name;
+        name=doughnutChartData.getInnerColumnChart[i].assetName.split(",",doughnutChartData.getInnerColumnChart.length+1);
+        this.namePushingArray.push(name);
+      }
+      // Converting into respective format
+      const perChunk = 2 // items per chunk 
+      for(let i=0;i<this.namePushingArray.length;i++){
+      let bean = {name:'',id:'',type:'column',data:[]}
+      const result = this.namePushingArray[i].reduce((resultArray, item, index) => { 
+          const chunkIndex = Math.floor(index/perChunk)
+          if(!resultArray[chunkIndex]) {
+            resultArray[chunkIndex] = [] // start a new chunk
+          }
+          // condition for odd index value converting into int
+          if(index%2==0){
+            resultArray[chunkIndex].push(item);
+          }else{
+            resultArray[chunkIndex].push(parseInt(item));
+          }
+        return resultArray
+      }, [])
+
+      bean.name=doughnutChartData.getInnerColumnChart[i].name;
+      bean.id=doughnutChartData.getInnerColumnChart[i].name;
+      bean.data=result;
+      this.columnInnerValueArray.push(bean);
+      }
+      // Calling the Drilldown chart
+      this.drilldownChart();
+
       this.flowChartFlag=true;
       }else{
         this.flowChartFlag=false;
@@ -641,8 +655,8 @@ configUserLog: {
       };
 
     // Column Chart
-    
-    chartOptionsColumnChart: Options = {
+    drilldownChart(){
+    this.chartOptionsColumnChart = {
       chart: {
         events: {
           drilldown: () => {
@@ -709,43 +723,7 @@ configUserLog: {
           type: 'column',
           name: 'Assets',
           colorByPoint: true,
-           data:[
-            {
-                name: 'Computer & Software',
-                y: 63.06,
-                drilldown: 'Chrome'
-            },
-            {
-                name: 'Furniture & Fittings',
-                y: 19.84,
-                drilldown: 'Safari'
-            },
-            {
-                name: 'Office Equipments',
-                y: 4.18,
-                drilldown: 'Firefox'
-            },
-            {
-                name: 'Plant & Machinery',
-                y: 4.12,
-                drilldown: 'Edge'
-            },
-            {
-                name: 'Vehicles',
-                y: 2.33,
-                drilldown: 'Opera'
-            },
-            {
-                name: 'Buildings',
-                y: 0.45,
-                drilldown: 'Internet Explorer'
-            },
-            {
-                name: 'Other',
-                y: 1.582,
-                drilldown: null
-            }
-        ]
+           data:this.columnOuterValueArray
         }
       ],
       drilldown: {
@@ -754,234 +732,48 @@ configUserLog: {
                 align: 'right'
             }
         },
-        series: [
-          {
-              name: 'Chrome',
-              id: 'Chrome',
-              type:'column',
-              data: [
-                  [
-                      'v65.0',
-                      0.1
-                  ],
-                  [
-                      'v64.0',
-                      1.3
-                  ],
-                  [
-                      'v63.0',
-                      53.02
-                  ],
-                  [
-                      'v62.0',
-                      1.4
-                  ],
-                  [
-                      'v61.0',
-                      0.88
-                  ],
-                  [
-                      'v60.0',
-                      0.56
-                  ],
-                  [
-                      'v59.0',
-                      0.45
-                  ],
-                  [
-                      'v58.0',
-                      0.49
-                  ],
-                  [
-                      'v57.0',
-                      0.32
-                  ],
-                  [
-                      'v56.0',
-                      0.29
-                  ],
-                  [
-                      'v55.0',
-                      0.79
-                  ],
-                  [
-                      'v54.0',
-                      0.18
-                  ],
-                  [
-                      'v51.0',
-                      0.13
-                  ],
-                  [
-                      'v49.0',
-                      2.16
-                  ],
-                  [
-                      'v48.0',
-                      0.13
-                  ],
-                  [
-                      'v47.0',
-                      0.11
-                  ],
-                  [
-                      'v43.0',
-                      0.17
-                  ],
-                  [
-                      'v29.0',
-                      0.26
-                  ]
-              ]
-          },
-          {
-              name: 'Firefox',
-              id: 'Firefox',
-              type:'column',
-              data: [
-                  [
-                      'v58.0',
-                      1.02
-                  ],
-                  [
-                      'v57.0',
-                      7.36
-                  ],
-                  [
-                      'v56.0',
-                      0.35
-                  ],
-                  [
-                      'v55.0',
-                      0.11
-                  ],
-                  [
-                      'v54.0',
-                      0.1
-                  ],
-                  [
-                      'v52.0',
-                      0.95
-                  ],
-                  [
-                      'v51.0',
-                      0.15
-                  ],
-                  [
-                      'v50.0',
-                      0.1
-                  ],
-                  [
-                      'v48.0',
-                      0.31
-                  ],
-                  [
-                      'v47.0',
-                      0.12
-                  ]
-              ]
-          },
-          {
-              name: 'Internet Explorer',
-              id: 'Internet Explorer',
-              type:'column',
-              data: [
-                  [
-                      'v11.0',
-                      6.2
-                  ],
-                  [
-                      'v10.0',
-                      0.29
-                  ],
-                  [
-                      'v9.0',
-                      0.27
-                  ],
-                  [
-                      'v8.0',
-                      0.47
-                  ]
-              ]
-          },
-          {
-              name: 'Safari',
-              id: 'Safari',
-              type:'column',
-              data: [
-                  [
-                      'v11.0',
-                      3.39
-                  ],
-                  [
-                      'v10.1',
-                      0.96
-                  ],
-                  [
-                      'v10.0',
-                      0.36
-                  ],
-                  [
-                      'v9.1',
-                      0.54
-                  ],
-                  [
-                      'v9.0',
-                      0.13
-                  ],
-                  [
-                      'v5.1',
-                      0.2
-                  ]
-              ]
-          },
-          {
-              name: 'Edge',
-              id: 'Edge',
-              type:'column',
-              data: [
-                  [
-                      'v16',
-                      2.6
-                  ],
-                  [
-                      'v15',
-                      0.92
-                  ],
-                  [
-                      'v14',
-                      0.4
-                  ],
-                  [
-                      'v13',
-                      0.1
-                  ]
-              ]
-          },
-          {
-              name: 'Opera',
-              id: 'Opera',
-              type:'column',
-              data: [
-                  [
-                      'v50.0',
-                      0.96
-                  ],
-                  [
-                      'v49.0',
-                      0.82
-                  ],
-                  [
-                      'v12.1',
-                      0.14
-                  ]
-              ]
-          }
-      ]
+        series:this.columnInnerValueArray
+      //   [
+      //     {
+      //         name: 'Internet Explorer',
+      //         id: 'Internet Explorer',
+      //         type:'column',
+      //         data: [
+      //             [
+      //                 'da',
+      //                 600
+      //             ],
+      //             [
+      //                 'v10.0',
+      //                 0.29
+      //             ],
+      //             [
+      //                 'v9.0',
+      //                 0.27
+      //             ]
+      //         ]
+      //     },
+      //     {
+      //         name: 'Opera',
+      //         id: 'Opera',
+      //         type:'column',
+      //         data: [
+      //             [
+      //                 'v49.0',
+      //                 0.82
+      //             ],
+      //             [
+      //                 'v12.1',
+      //                 0.14
+      //             ]
+      //         ]
+      //     }
+      // ]
+
     } as Highcharts.DrilldownOptions
     };
 
-
+  }
 
   chartCallback: Highcharts.ChartCallbackFunction = chart => {
     this.chartRef = chart;
