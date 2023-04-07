f5fcedd2c9e9f0eec675e69261d918fda405df0e
Riyaz Ahamed K <riyazahamed1444@gmail.com>
2023-04-07 15:10:36 +0530

    Company ifsc validation

--- src/app/master/company/add-company/add-company.component.ts
+++ src/app/master/company/add-company/add-company.component.ts
@@ -102,7 +102,7 @@ export class AddCompanyComponent implements OnInit {
       webSite: ["", Validators.pattern('^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$')],
       panNo: ["", Validators.pattern('[A-Z]{5}[0-9]{4}[A-Z]{1}')],
       gstNo: ["", Validators.pattern('[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Za-z]{1}[Z]{1}[0-9A-Za-z]{1}')],
-      ifscCode: ["", Validators.pattern('[A-Za-z]{4}[0-9]{7}')],
+      ifscCode: ["", Validators.pattern('[A-Za-z]{4}[A-Z0-9]{7}')],
       country: ["", [Validators.required]],
       isactive: [true],
       depreciation:[""],
