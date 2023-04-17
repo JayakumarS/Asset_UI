cd86f07b0178bf8a5829bf450bc7a9690a1b7f1a
Kannan-DCI <kannanp@paragondynamics.in>
2023-04-17 19:31:06 +0530

    speak to text changes

--- src/app/angular-bot/chat.service.ts
+++ src/app/angular-bot/chat.service.ts
@@ -45,44 +45,44 @@ export class ChatService {
 
 
   /*************************For Speak To Text********** */
-  start() {
-    this.isStoppedSpeechRecog = false;
-    this.recognition.start();
-    console.log("Speech recognition started")
-    this.recognition.addEventListener('end', () => {
-      if (this.isStoppedSpeechRecog) {
-        this.recognition.stop();
-        console.log("End speech recognition")
-      } else {
-        this.wordConcat()
-        this.recognition.start();
-      }
-    });
-  }
+  // start() {
+  //   this.isStoppedSpeechRecog = false;
+  //   this.recognition.start();
+  //   console.log("Speech recognition started")
+  //   this.recognition.addEventListener('end', () => {
+  //     if (this.isStoppedSpeechRecog) {
+  //       this.recognition.stop();
+  //       console.log("End speech recognition")
+  //     } else {
+  //       this.wordConcat()
+  //       this.recognition.start();
+  //     }
+  //   });
+  // }
 
-  stop() {
-    this.isStoppedSpeechRecog = true;
-    this.wordConcat()
-    this.recognition.stop();
-    console.log("End speech recognition")
-  }
+  // stop() {
+  //   this.isStoppedSpeechRecog = true;
+  //   this.wordConcat()
+  //   this.recognition.stop();
+  //   console.log("End speech recognition")
+  // }
 
   
-  wordConcat() {
-    this.text = this.text + ' ' + this.tempWords + '.';
-    this.tempWords = '';
-  }
-  init() {
-    this.recognition.interimResults = true;
-    this.recognition.lang = 'en-US';
-    this.recognition.addEventListener('result', (e: { results: Iterable<unknown> | ArrayLike<unknown>; }) => {
-      const transcript = Array.from(e.results)
-        .map((result) => result[0])
-        .map((result) => result.transcript)
-        .join('');
-      this.tempWords = transcript;
-      console.log(transcript);
-    });
-  }
+  // wordConcat() {
+  //   this.text = this.text + ' ' + this.tempWords + '.';
+  //   this.tempWords = '';
+  // }
+  // init() {
+  //   this.recognition.interimResults = true;
+  //   this.recognition.lang = 'en-US';
+  //   this.recognition.addEventListener('result', (e: { results: Iterable<unknown> | ArrayLike<unknown>; }) => {
+  //     const transcript = Array.from(e.results)
+  //       .map((result) => result[0])
+  //       .map((result) => result.transcript)
+  //       .join('');
+  //     this.tempWords = transcript;
+  //     console.log(transcript);
+  //   });
+  // }
   
 }
